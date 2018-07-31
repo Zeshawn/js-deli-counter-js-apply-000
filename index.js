f3196b25b7f458e68e7a44d59cfb3bd8d7aa569e
Zeshawn Khan <zeshawnk@gmail.com>
2018-07-31 15:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing()
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length == 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var served = katzDeliLine.shift(0);
+    return served
+  }
+}
